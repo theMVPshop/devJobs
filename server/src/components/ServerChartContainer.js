eafae55c39297c8dbc5b74717ea0f9687ae19f29
taplinseth <taplinseth@gmail.com>
2023-03-30 19:48:36 -0500

    server stuff

--- server/src/components/ServerChartContainer.js
+++ server/src/components/ServerChartContainer.js
@@ -1,69 +1,67 @@
 import React from "react";
 import ServerChart from "./ServerChart";
-
+import { executeScrape } from "../scraper/jobSearch";
 export default class ServerChartContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       jobData: [],
       loading: true,
-      error: false,
+      error: null,
       searchParams: {
         searchId: 1,
         cursor: 0,
         count: 100,
       },
     };
-    this.fetchJobData = this.fetchJobData.bind(this);
   }
 
-  componentDidMount() {
-    this.fetchJobData();
+  async componentDidMount() {
+    await this.fetchJobData();
+    executeScrape();
   }
 
-  fetchJobData() {
+  fetchJobData = async () => {
     const TOKEN = process.env.REACT_APP_TOKEN;
     const { searchId, cursor, count } = this.state.searchParams;
 
-    fetch(
-      `https://learning.careers/version-test/api/1.1/obj/jobData?searchId=${searchId}&cursor=${cursor}&count=${count}`,
-      {
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const { jobData } = this.state;
-        const newJobData = jobData.concat(data.response.results);
-        const remaining = data.response.remaining;
-
-        if (remaining > 0) {
-          this.setState(
-            {
-              jobData: newJobData,
-              loading: true,
-              searchParams: {
-                ...this.state.searchParams,
-                cursor: cursor + count,
-              },
-            },
-            () => this.fetchJobData(),
-          );
-        } else {
-          this.setState({ jobData: newJobData, loading: false });
+    try {
+      const response = await fetch(
+        `https://learning.careers/version-test/api/1.1/obj/jobData?searchId=${searchId}&cursor=${cursor}&count=${count}`,
+        {
+          headers: {
+            Authorization: `Bearer ${TOKEN}`,
+            "Content-Type": "application/json",
+          },
         }
-      })
-      .catch((error) => {
-        console.error(error);
-        this.setState({ error: true, loading: false });
-      });
-  }
+      );
+      const data = await response.json();
+
+      this.setState(
+        (prevState) => ({
+          jobData: [...prevState.jobData, ...data.response.results],
+          loading: data.response.remaining > 0,
+          searchParams: {
+            ...prevState.searchParams,
+            cursor:
+              prevState.searchParams.cursor + prevState.searchParams.count,
+          },
+        }),
+      );
+
+      if (data.response.remaining > 0) {
+        await this.fetchJobData();
+      }
+    } catch (error) {
+      console.error(error);
+      this.setState({ error: true, loading: false });
+    }
+  };
 
   render() {
     const { jobData, loading, error } = this.state;
+    const filteredJobData = jobData.filter((data) => data.searchId === 1);
+
     return (
       <div className="chartHeaderContainer">
         <div className="chartHeader">
@@ -76,7 +74,7 @@ export default class ServerChartContainer extends React.Component {
         <div className="serverChartContainer">
           {loading && <p>Loading...</p>}
           {error && <p>Error fetching data.</p>}
-          {!loading && !error && <ServerChart jobData={jobData} />}
+          {!loading && !error && <ServerChart jobData={filteredJobData} />}
         </div>
       </div>
     );
