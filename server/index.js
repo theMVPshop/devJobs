f8be14118c11054ee725136d43276b17ba08e799
kkritselis <kkritselis@gmail.com>
2023-04-13 18:53:16 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { executeScrape } = require("./jobSearch.js");
 
 const app = express();
 const port = process.env.PORT || 3006;
-
+console.log("index prescrape");
 app.get("/scrape", async (req, res) => {
   executeScrape();
 });
